5dd2307e3ecc74a5341587b86ca5924798bcdf5d
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-11-07 21:00:11 -0500

    chats ya no se duplican

--- lib/core/utils/chat_utils.dart
+++ lib/core/utils/chat_utils.dart
@@ -1,10 +1,12 @@
 class ChatUtils {
-  static String getChatRoomIdByUsername(String a, String b) {
-    if (a.substring(0, 1).codeUnitAt(0) > b.substring(0, 1).codeUnitAt(0)) {
+static String getChatRoomIdByUsername(String a, String b) {
+    // Comparamos las cadenas completas. 
+    // compareTo devuelve > 0 si 'a' es "mayor" que 'b' alfabéticamente.
+     if (a.compareTo(b) > 0) {
       return "$b\_$a";
     } else {
-      return "$a\_$b";
-    }
+       return "$a\_$b";
+   }
   }
 
   static String formatTime(DateTime dateTime) {
